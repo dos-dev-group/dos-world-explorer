521cb3d45fa024a8cea64d14c0da0b70577a7ca1
cdwdong <cdwdong1@gmail.com>
2022-06-04 17:13:38 +0900

    chore: 수정모달 태그 관련 코드 수정

--- src/renderer/features/search/EditWorldModal.tsx
+++ src/renderer/features/search/EditWorldModal.tsx
@@ -171,11 +171,8 @@ function EditWorldModal(props: Props) {
       <div css={{ marginTop: spacing(1), width: 'calc(100% - 200px)' }}>
         <Typography.Title level={5}>태그</Typography.Title>
         {curTags.map((tag) => {
-          let color;
-          if (tag) {
-            const colorIndex = simpleStringHash(tag) % PresetColorTypes.length;
-            color = PresetColorTypes[colorIndex];
-          }
+          const colorIndex = simpleStringHash(tag) % PresetColorTypes.length;
+          const color = PresetColorTypes[colorIndex];
 
           return (
             <Tag
