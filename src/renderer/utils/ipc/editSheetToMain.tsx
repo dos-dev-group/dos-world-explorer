3148a80d4d93dd7838af6ccaadc9bffcf7a2b84a
lkmisem <lkmisem@gmail.com>
2022-09-09 04:49:40 +0900

    feat: tagData, checker_sheet의 가져어기,추가,수정,삭제 기능 구현

--- src/renderer/utils/ipc/editSheetToMain.tsx
+++ src/renderer/utils/ipc/editSheetToMain.tsx
@@ -5,6 +5,10 @@ import {
   WorldEditOutput,
   EditResult,
   SheetBaseType,
+  TagStyle,
+  TagStyleInput,
+  CheckerWorldEditInput,
+  CheckerWorldData,
 } from '@src/types';
 import { NoDataError } from '../error';
 
@@ -32,32 +36,26 @@ export function getWorldDataToMain() {
   });
 }
 
+export function getCheckerWorldDataToMain() {
+  console.log('getCheckerWorldDataToMain test');
+  window.electron.ipcRenderer.sendMessage('getCheckerWorldDataToMain', []);
+  return new Promise<CheckerWorldData>((resolve, reject) => {
+    window.electron.ipcRenderer.once(
+      'getCheckerWorldDataToRenderer',
+      (result: unknown) => {
+        resolve(result as CheckerWorldData);
+      },
+    );
+  });
+}
+
 export function addSheetToMain(
-  type: 'World' | 'SuggestWorld' | 'TagStyle',
+  type: 'World' | 'CheckerWorld' | 'TagStyle',
   input: any,
 ) {
   window.electron.ipcRenderer.sendMessage('addSheetToMain', [type, input]);
 
-  return new Promise<EditResult>((resolve, reject) => {
-    window.electron.ipcRenderer.once(
-      'addSheetToRenderer',
-      (result: unknown) => {
-        const er = result as EditResult;
-        if (er === EditResult.ALREADYEXIST) {
-          reject(new Error('이미 존재합니다.'));
-        } else if (er === EditResult.NOTEXIST) {
-          reject(new Error('존재하지 않습니다.'));
-        } else if (er === EditResult.PROTECTED) {
-          reject(new Error('다른곳에서 먼저 사용되고 있습니다.'));
-        } else if (er === EditResult.TYPEERROR) {
-          reject(new Error('타입이 맞지않습니다'));
-        } else if (er === EditResult.UNKNOWN) {
-          reject(new Error('알 수 없는 오류 발생'));
-        }
-        resolve(er);
-      },
-    );
-  });
+  return returnEditResult('addSheetToMain');
 }
 
 export function addEditSheetToMain(worldInput: WorldEditInput) {
@@ -67,26 +65,30 @@ export function addEditSheetToMain(worldInput: WorldEditInput) {
     'addEditSheetToRenderer',
   ]);
 
-  return new Promise<EditResult>((resolve, reject) => {
-    window.electron.ipcRenderer.once(
-      'addEditSheetToRenderer',
-      (result: unknown) => {
-        const er = result as EditResult;
-        if (er === EditResult.ALREADYEXIST) {
-          reject(new Error('이미 존재합니다.'));
-        } else if (er === EditResult.NOTEXIST) {
-          reject(new Error('존재하지 않습니다.'));
-        } else if (er === EditResult.PROTECTED) {
-          reject(new Error('다른곳에서 먼저 사용되고 있습니다.'));
-        } else if (er === EditResult.TYPEERROR) {
-          reject(new Error('타입이 맞지않습니다'));
-        } else if (er === EditResult.UNKNOWN) {
-          reject(new Error('알 수 없는 오류 발생'));
-        }
-        resolve(er);
-      },
-    );
-  });
+  return returnEditResult('addEditSheetToRenderer');
+}
+
+export function addTagStyleSheetToMain(tagStyleInput: TagStyleInput) {
+  window.electron.ipcRenderer.sendMessage('addSheetToMain', [
+    'TagStyle',
+    tagStyleInput,
+    'addTagStyleSheetToRenderer',
+  ]);
+
+  return returnEditResult('addTagStyleSheetToRenderer');
+}
+
+export function addCheckerWorldSheetToMain(
+  checkerWorldInput: CheckerWorldEditInput,
+) {
+  window.electron.ipcRenderer.sendMessage('addSheetToMain', [
+    'CheckerWorld',
+    checkerWorldInput,
+    'addCheckerWorldSheetToRenderer',
+  ]);
+
+  return returnEditResult('addCheckerWorldSheetToRenderer');
+  // return 'a';
 }
 
 export function reomoveEditSheetToMain(key: string) {
@@ -96,26 +98,27 @@ export function reomoveEditSheetToMain(key: string) {
     'reomoveEditSheetToRenderer',
   ]);
 
-  return new Promise<EditResult>((resolve, reject) => {
-    window.electron.ipcRenderer.once(
-      'reomoveEditSheetToRenderer',
-      (result: unknown) => {
-        const er = result as EditResult;
-        if (er === EditResult.ALREADYEXIST) {
-          reject(new Error('이미 존재합니다.'));
-        } else if (er === EditResult.NOTEXIST) {
-          reject(new Error('존재하지 않습니다.'));
-        } else if (er === EditResult.PROTECTED) {
-          reject(new Error('다른곳에서 먼저 사용되고 있습니다.'));
-        } else if (er === EditResult.TYPEERROR) {
-          reject(new Error('타입이 맞지않습니다'));
-        } else if (er === EditResult.UNKNOWN) {
-          reject(new Error('알 수 없는 오류 발생'));
-        }
-        resolve(er);
-      },
-    );
-  });
+  return returnEditResult('reomoveEditSheetToRenderer');
+}
+
+export function removeCheckerWorldSheetToMain(key: string) {
+  window.electron.ipcRenderer.sendMessage('reomoveSheetToMain', [
+    'CheckerWorld',
+    key,
+    'removeCheckerWorldSheetToRenderer',
+  ]);
+
+  return returnEditResult('removeCheckerWorldSheetToRenderer');
+}
+
+export function removeTagStyleSheetToMain(key: string) {
+  window.electron.ipcRenderer.sendMessage('reomoveSheetToMain', [
+    'TagStyle',
+    key,
+    'reomoveTagStyleSheetToRenderer',
+  ]);
+
+  return returnEditResult('reomoveTagStyleSheetToRenderer');
 }
 
 export function modifyEditSheetToMain(key: string, worldInput: WorldEditInput) {
@@ -126,9 +129,53 @@ export function modifyEditSheetToMain(key: string, worldInput: WorldEditInput) {
     'modifyEditSheetToRenderer',
   ]);
 
+  return returnEditResult('modifyEditSheetToRenderer');
+}
+
+export function modifyTagStyleSheetToMain(
+  key: string,
+  tagStyle: TagStyleInput,
+) {
+  window.electron.ipcRenderer.sendMessage('modifySheetToMain', [
+    'TagStyle',
+    key,
+    tagStyle,
+    'modifyTagStyleSheetToRenderer',
+  ]);
+
+  return returnEditResult('modifyTagStyleSheetToRenderer');
+}
+
+export function modifyCheckerWorldSheetToMain(
+  key: string,
+  checkerWorldInput: CheckerWorldEditInput,
+) {
+  window.electron.ipcRenderer.sendMessage('modifySheetToMain', [
+    'CheckerWorld',
+    key,
+    checkerWorldInput,
+    'modifyCheckerWorldSheetToRenderer',
+  ]);
+
+  return returnEditResult('modifyCheckerWorldSheetToRenderer');
+}
+
+export function autoFileToMain(worldUrl: string) {
+  window.electron.ipcRenderer.sendMessage('autoFileToMain', [worldUrl]);
+  return new Promise<WorldEditOutput>((resolve, reject) => {
+    window.electron.ipcRenderer.once(
+      'autoFileToRenderer',
+      (result: unknown) => {
+        resolve(result as WorldEditOutput);
+      },
+    );
+  });
+}
+
+function returnEditResult(ipcRendererName: string) {
   return new Promise<EditResult>((resolve, reject) => {
     window.electron.ipcRenderer.once(
-      'modifyEditSheetToRenderer',
+      'modifyTagStyleSheetToRenderer',
       (result: unknown) => {
         const er = result as EditResult;
         if (er === EditResult.ALREADYEXIST) {
@@ -143,18 +190,6 @@ export function modifyEditSheetToMain(key: string, worldInput: WorldEditInput) {
           reject(new Error('알 수 없는 오류 발생'));
         }
         resolve(er);
-      },
-    );
-  });
-}
-
-export function autoFileToMain(worldUrl: string) {
-  window.electron.ipcRenderer.sendMessage('autoFileToMain', [worldUrl]);
-  return new Promise<WorldEditOutput>((resolve, reject) => {
-    window.electron.ipcRenderer.once(
-      'autoFileToRenderer',
-      (result: unknown) => {
-        resolve(result as WorldEditOutput);
       },
     );
   });
