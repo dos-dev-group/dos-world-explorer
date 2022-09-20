a86f2a8e486790278de5619bb09720b7c4aced10
lkmisem <lkmisem@gmail.com>
2022-09-20 20:05:08 +0900

    fix: returnEditResult()함수에 상수값이 박혀있던 오류 수정

--- src/renderer/utils/ipc/editSheetToMain.tsx
+++ src/renderer/utils/ipc/editSheetToMain.tsx
@@ -188,23 +188,20 @@ export function autoFileToMain(worldUrl: string) {
 
 function returnEditResult(ipcRendererName: string) {
   return new Promise<EditResult>((resolve, reject) => {
-    window.electron.ipcRenderer.once(
-      'modifyTagStyleSheetToRenderer',
-      (result: unknown) => {
-        const er = result as EditResult;
-        if (er === EditResult.ALREADYEXIST) {
-          reject(new Error('이미 존재합니다.'));
-        } else if (er === EditResult.NOTEXIST) {
-          reject(new Error('존재하지 않습니다.'));
-        } else if (er === EditResult.PROTECTED) {
-          reject(new Error('다른곳에서 먼저 사용되고 있습니다.'));
-        } else if (er === EditResult.TYPEERROR) {
-          reject(new Error('타입이 맞지않습니다'));
-        } else if (er === EditResult.UNKNOWN) {
-          reject(new Error('알 수 없는 오류 발생'));
-        }
-        resolve(er);
-      },
-    );
+    window.electron.ipcRenderer.once(ipcRendererName, (result: unknown) => {
+      const er = result as EditResult;
+      if (er === EditResult.ALREADYEXIST) {
+        reject(new Error('이미 존재합니다.'));
+      } else if (er === EditResult.NOTEXIST) {
+        reject(new Error('존재하지 않습니다.'));
+      } else if (er === EditResult.PROTECTED) {
+        reject(new Error('다른곳에서 먼저 사용되고 있습니다.'));
+      } else if (er === EditResult.TYPEERROR) {
+        reject(new Error('타입이 맞지않습니다'));
+      } else if (er === EditResult.UNKNOWN) {
+        reject(new Error('알 수 없는 오류 발생'));
+      }
+      resolve(er);
+    });
   });
 }
