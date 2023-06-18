d15fb5afe886a5834abcd3b6cac3539a8de88edd
cdwdong <cdwdong1@gmail.com>
2023-06-18 19:58:06 +0900

    fix: fileUtils 불러오기 함수 처리 오류 수정

--- src/renderer/utils/ipc/fileUtils.tsx
+++ src/renderer/utils/ipc/fileUtils.tsx
@@ -2,6 +2,12 @@ import packageMetadata from '@src/../package.json';
 
 const APP_NAME = packageMetadata.name;
 
+interface FileData {
+  app: string;
+  type: string;
+  data: unknown;
+}
+
 export function showSaveFileDialog<T = unknown>(type: string, target: T) {
   window.electron.ipcRenderer.sendMessage('showSaveFileDialog', [
     {
@@ -25,20 +31,21 @@ export function showLoadFileDialog<T = unknown>(type: string) {
   window.electron.ipcRenderer.sendMessage('showLoadFileDialog', []);
   return new Promise<T>((resolve, reject) => {
     window.electron.ipcRenderer.once('showLoadFileDialog', (result: any) => {
-      if (result === null) {
+      const fileData = result as FileData;
+      if (fileData === null) {
         reject(new Error('Canceled'));
         return;
       }
-      if (result?.app !== APP_NAME) {
+      if (fileData?.app !== APP_NAME) {
         reject(new Error('Not matched this app'));
         return;
       }
-      if (result?.type !== type) {
+      if (fileData?.type !== type) {
         reject(new Error('Not matched type'));
         return;
       }
 
-      resolve(result as T);
+      resolve(fileData.data as T);
     });
   });
 }
